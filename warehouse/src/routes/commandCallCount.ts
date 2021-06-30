3c563724642c39a17cd73508a349a6deaf7998ef
crowbardispensingcore <crowbargel@gmail.com>
2021-06-30 10:08:09 -0700

    Query method change

--- warehouse/src/routes/commandCallCount.ts
+++ warehouse/src/routes/commandCallCount.ts
@@ -6,45 +6,58 @@ import { DatabaseConnection } from '../utils/DB';
 const router = express.Router();
 
 router.get('/commandCallCount', async (req: Request, res: Response) => {
-    if (req.query.command == undefined) {
-        return res.status(400).send('Command not found!');
+  if (req.query.command == undefined) {
+    return res.status(400).send('Command not found!');
+  }
+
+  let command: string = req.query.command as string;
+
+  if (command == null) {
+    return res.status(400).send('Command not found!');
+  }
+
+  const db = new DatabaseConnection();
+  await db.connect();
+
+  // command = db.connection?.escape(command) || '';
+
+  const sql = 
+    `
+    SELECT 
+      COUNT(*) AS callCount
+    FROM 
+      Event
+    WHERE (
+      EventSource = 22
+      AND ATTR_1 = 'Successful command call'
+      AND ATTR_2 = ?
+    );
+    `;
+  
+  db.connection?.query(sql, [command], (error, results) => {
+    if (error) {
+      return res.status(500).send(error);
     }
-
-    let command: string = req.query.command as string;
-
-    if (command == null) {
-        return res.status(400).send('Command not found!');
+    else {
+      const callCount = results[0].callCount;
+      res.json({
+        "Command": command, 
+        "Call Count": callCount
+      });
     }
+  });
 
-    const db = new DatabaseConnection();
-    await db.connect();
-
-    command = db.connection?.escape(command) || '';
-
-    const sql = 
-        `
-        SELECT 
-            COUNT(*) AS callCount
-        FROM 
-            Event
-        WHERE (
-            EventSource = 22
-            AND ATTR_1 = 'Successful command call'
-            AND ATTR_2 = ${command}
-        );
-        `;
-    
-    db.query(sql)
-        .then(results => {
-            const count = results[0].callCount;
-            res.json({
-              "Command": command, 
-              "Call Count": count
-            });
-        })
-        .catch (err => {
-            return res.status(500).send(err);
-        });
+  // db.query(sql)
+  //   .then(results => {
+  //     const count = results[0].callCount;
+  //     res.json({
+  //       "Command": command, 
+  //       "Call Count": count
+  //     });
+  //   })
+  //   .catch (err => {
+  //     return res.status(500).send(err);
+  //   });
 });
 
 module.exports = router;
