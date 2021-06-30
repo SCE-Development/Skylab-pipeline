16b9ac6962ccebf9e1188637e5d95d8c88451db6
Anshul Jha <65700919+ajha7@users.noreply.github.com>
2021-06-29 21:15:57 -0700

    Added hardcoded column values elEndpoint

--- warehouse/src/routes/etlEndpoint.ts
+++ warehouse/src/routes/etlEndpoint.ts
@@ -2,21 +2,6 @@ import express from "express";
 import { DatabaseConnection } from "../utils/DB";
 const router = express.Router();
 const CONNECTION = new DatabaseConnection();
-import {
-    EVENTSOURCE,
-    USERID,
-    SSOID,
-    EVENTDATE,
-    EVENTTS,
-    EVENTDESCRIPTION,
-    EVENTERROR,
-    ATTR_1,
-    ATTR_2,
-    ATTR_3,
-    ATTR_4,
-    ATTR_5,
-    ATTR_JSON
-} from "../config/constants.json";
 
 /*
     Inserts row into Event table with given parameters
@@ -77,20 +62,20 @@ router.get("/etlEndpoint", async (req, res) => {
     } = req.body;
 
     const eventTableColumns = new Map<string, any>();
-
-    eventTableColumns.set(EVENTSOURCE, eventSource);
-    eventTableColumns.set(USERID,userID);
-    eventTableColumns.set(SSOID, ssoid);
-    eventTableColumns.set(EVENTDATE, eventDate);
-    eventTableColumns.set(EVENTTS, eventTS);
-    eventTableColumns.set(EVENTDESCRIPTION, eventDescription);
-    eventTableColumns.set(EVENTERROR, eventError);
-    eventTableColumns.set(ATTR_1, attr_1);
-    eventTableColumns.set(ATTR_2, attr_2);
-    eventTableColumns.set(ATTR_3, attr_3);
-    eventTableColumns.set(ATTR_4, attr_4);
-    eventTableColumns.set(ATTR_5, attr_5);
-    eventTableColumns.set(ATTR_JSON, attr_JSON);
+    
+    eventTableColumns.set("EventSource", eventSource);
+    eventTableColumns.set("UserID",userID);
+    eventTableColumns.set("SSOID", ssoid);
+    eventTableColumns.set("EventDate", eventDate);
+    eventTableColumns.set("EventTS", eventTS);
+    eventTableColumns.set("EventDescription", eventDescription);
+    eventTableColumns.set("EventError", eventError);
+    eventTableColumns.set("ATTR_1", attr_1);
+    eventTableColumns.set("ATTR_2", attr_2);
+    eventTableColumns.set("ATTR_3", attr_3);
+    eventTableColumns.set("ATTR_4", attr_4);
+    eventTableColumns.set("ATTR_5", attr_5);
+    eventTableColumns.set("ATTR_JSON", attr_JSON);
 
     await CONNECTION?.connect();
 
